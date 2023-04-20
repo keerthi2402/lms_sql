0dc2c26bc7ed54df75c73d40d53061afdedf0127
keerthi2402 <114648315+keerthi2402@users.noreply.github.com>
2023-04-20 12:44:20 +0530

    Create select.sql

---
+++ select.sql
@@ -0,0 +1,17 @@
+select * from branch
+
+select * from customer
+
+select * from Authentication_system
+
+select * from publisher
+
+select * from author
+
+select * from staff
+
+select * from book
+
+slect * from updates
+
+select * from issue_status
